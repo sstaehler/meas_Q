e857b3d930fb76b888f96ab1454e843b158efb09
sstaehler <simon.staehler@erdw.ethz.ch>
2020-03-12 17:01:12 +0100

    Rename package consistently

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ Python tool to predict tstar, given a Taup file and a travel time
 '''
 from setuptools import setup, find_packages
 
-setup(name='mqs_reports',
+setup(name='pred_tstar',
       version='0.1',
       description='Python tool to predict tstar given.',
-      url='https://github.com/sstaehler/meas_Q',
+      url='https://github.com/sstaehler/pred_tstar',
       author='Simon Stähler',
       author_email='staehler@erdw.ethz.ch',
       license='None',
